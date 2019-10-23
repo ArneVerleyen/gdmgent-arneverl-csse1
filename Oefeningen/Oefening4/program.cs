0566930877d8a8cc73b14a5992ad1a1671f0c59a
ArneVerleyen <arneverl@student.arteveldehs.be>
2019-10-23 21:25:30 +0200

    oefening4

---
+++ Oefeningen/Oefening4/program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Rekenmachine {
+  class Program {
+    static void Main(string[] args) {
+      int getal1 = 8;
+      int getal2 = 3;
+      Console.WriteLine(getal1+getal2);
+      Console.WriteLine(getal1-getal2);
+      Console.WriteLine(getal1*getal2);
+      Console.WriteLine(getal1/getal2);
+      console.readLine();
+
+    }
+  }
+}
